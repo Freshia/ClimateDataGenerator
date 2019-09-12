c6256500926345136b5b2bd5fb4dbc0953ec6392
Freshia <freshiasackey@gmail.com>
2019-09-12 12:16:46 +0300

    Added scons. Changes file structure

---
+++ SConstruct
@@ -0,0 +1,2 @@
+Library('generator',['./lib/generator.c'])
+Program('./src/test.c', LIBS=['generator','m'],LIBPATH='.')
--- src/test.c
+++ src/test.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-#include "generator.h"
+#include<math.h>
+#include<time.h>
+#include "../lib/generator.h"
 
 //double * pressure_data (int);
 int main(){
+    srand( (unsigned)time( NULL ) ); 
     double *x;
     x = pressure_data(5);
     for (int i=0;i<5;i++){
